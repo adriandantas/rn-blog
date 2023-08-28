0d965813b593b85d298310411213f5db6bb41a3e
Adrian Dantas <adrian.dantas@gmail.com>
2023-08-27 21:03:59 -0300

    Initial version of Show Screen.

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,27 +3,29 @@ import {View, Text, StyleSheet, FlatList, Button, TouchableOpacity} from "react-
 import {Context} from "../context/BlogContext";
 import {Feather} from '@expo/vector-icons';
 
-const IndexScreen = (props) => {
-    const {state, addBlogPost, deleteBlogPost } = useContext(Context);
+const IndexScreen = ({navigation}) => {
+    const {state, addBlogPost, deleteBlogPost} = useContext(Context);
     return (
         <View>
-
             <Button title="Add Post" onPress={addBlogPost}/>
             <FlatList data={state}
                       keyExtractor={(blogPost) => blogPost.id}
                       renderItem={({item}) => {
                           return (
-                              <View style={styles.row}>
-                                  <Text style={styles.title}>{item.title} - {item.id}</Text>
-                                  <TouchableOpacity onPress={() => {
-                                      deleteBlogPost(item.id)
-                                  }}>
-                                      <Feather name="trash" style={styles.icon} color="black"/>
-                                  </TouchableOpacity>
-                              </View>
+                              <TouchableOpacity onPress={() => {
+                                 navigation.navigate('Show', {id: item.id});
+                              }}>
+                                  <View style={styles.row}>
+                                      <Text style={styles.title}>{item.title} - {item.id}</Text>
+                                      <TouchableOpacity onPress={() => {
+                                          deleteBlogPost(item.id)
+                                      }}>
+                                          <Feather name="trash" style={styles.icon} color="black"/>
+                                      </TouchableOpacity>
+                                  </View>
+                              </TouchableOpacity>
                           );
                       }}
-
             />
         </View>
     );
